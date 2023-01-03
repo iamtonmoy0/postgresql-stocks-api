52f38267f7caad595b4cd932f063136379f51d98
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-04 03:07:57 +0600

    gorilla mux imported

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-postgres-yt
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- router/router.go
+++ router/router.go
@@ -1,1 +1,12 @@
 package router
+
+import "github.com/gorilla/mux"
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.Handlefunc("/")
+	router.Handlefunc("/")
+	router.Handlefunc("/")
+	router.Handlefunc("/")
+	router.Handlefunc("/")
+}
